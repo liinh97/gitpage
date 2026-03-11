abb998e7467ee4c90bc2e338ffa65b858b8c064c
liinh97 <81209040+liinh97@users.noreply.github.com>
2026-03-11 11:43:40 +0700

    Update stats.js

--- assets/js/modules/stats.js
+++ assets/js/modules/stats.js
@@ -16,9 +16,13 @@ const DEFAULT_BASE_COST = 2000;
 // cost = 0 => bán hộ
 const ITEM_COST_OVERRIDES = {
   "Nem TCC": 2750,
+  "Sống Nem TCC": 2700,
   "Nem TCC xù": 2875,
+  "Sống Nem TCC xù": 2875,
   "Nem TCC vỏ giòn": 3050,
+  "Sống Nem TCC vỏ giòn": 3050,
   "Nem TCC phomai": 6816,
+  "Sống Nem TCC phomai": 6816,
   "Bánh rán mặn": 3630,
   "Bánh rán phomai": 3414,
   "Bánh rán cốm dừa": 0,
